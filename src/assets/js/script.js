fcab26efed35799a6fd028797c483345f2060f3e
Mauritz Marais <mauritz303@gmail.com>
2024-02-03 09:10:55 +0200

    implemented image viewer functionality

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -23,10 +23,68 @@ const faqItems = document.querySelectorAll(".faq-container .faq-item");
 const faqAnswers = document.querySelectorAll(".faq-item p");
 const faqIcons = document.querySelectorAll(".faq-item img");
 
-for(let i = 0; i < faqItems.length; i++) {
+for (let i = 0; i < faqItems.length; i++) {
     faqItems[i].addEventListener("click", () => {
         faqAnswers[i].classList.toggle("hidden");
 
         faqIcons[i].classList.toggle("rotate");
     });
 };
+
+
+// image viewer modal
+// click on image, change src of modal image, change styles of modal,
+const galleryImages = document.querySelectorAll("#gallery .image-container img");
+const modalImage = document.querySelector(".image-viewer .current-image img");
+const imageViewer = document.querySelector(".image-viewer");
+const closeViewerButton = document.querySelector(".image-viewer .close-viewer");
+
+// adjust position where modal pops up*
+const imageSrcArray = ['http://localhost:8080/assets/images/travel-1.jpg',
+ 'http://localhost:8080/assets/images/travel-2.jpg',
+  'http://localhost:8080/assets/images/travel-3.jpg',
+   'http://localhost:8080/assets/images/travel-4.jpg'];
+
+let current_index;
+
+galleryImages.forEach(image => {
+    image.addEventListener("click", (e) => {
+        // find index of image in array
+        current_index = imageSrcArray.indexOf(image.src);
+
+        // change modal image src
+        modalImage.src = imageSrcArray[current_index];
+
+        // change modal styles
+        imageViewer.classList.add("show-viewer");
+    });
+});
+
+// fix button clicking (its clicking on image)*
+
+// change image with left and right buttons
+imageViewer.addEventListener("click", (e) => {
+    
+    
+    // left
+    if(e.target.matches(".image-viewer .arrow-left")) {
+        // update current index
+        current_index === 0 ? current_index = imageSrcArray.length - 1 : current_index --;
+        modalImage.src = imageSrcArray[current_index];
+    };
+
+    // right
+    if(e.target.matches(".image-viewer .arrow-right")) {
+        // update current index
+        current_index === imageSrcArray.length - 1 ? current_index = 0 : current_index ++;
+        modalImage.src = imageSrcArray[current_index];
+    };
+
+    // close
+    if(e.target.matches(".image-viewer .close-viewer")) {
+        
+        imageViewer.classList.remove("show-viewer");
+    };
+});
+
+
